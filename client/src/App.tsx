8d9603ed960cea7fbf3b247412e29dbd2fb4f128
Panelkaa <xxx_rustam7_xxx@mail.ru>
2024-03-25 23:19:04 +0300

    Client part registration and login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import app from "./App.module.css"
-import './App.css'
+import { FC, useContext, useEffect } from "react"
+import LoginForm from "./components/LoginForm"
+import { Context } from "./main"
+import { observer } from "mobx-react-lite";
+import RegistrationForm from "./components/RegistrationForm";
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// eslint-disable-next-line react-refresh/only-export-components
+const App: FC = () => {
+  const {store} = useContext(Context);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+  useEffect(() => {
+    if(localStorage.getItem('token')) [
+      store.checkAuth()
+    ]
+  }, [store])
+
+  if(!store.isAuth) {
+    
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/registration" element={<RegistrationForm />}/>
+          <Route path="/" element={<LoginForm />}/>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+  return ( 
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <h1 className="text-xl mb-8 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          {store.isAuth ? `Пользователь авторизован ${store.user.email}`: 'Авторизуйтесь'}
+        </h1>
+        <button 
+          className="w-64 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          onClick={() => store.logout()}>
+            Выйти
         </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+// eslint-disable-next-line react-refresh/only-export-components
+export default observer(App); 
